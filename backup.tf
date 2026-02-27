9c3f01755042d8eeb74f4cff1ffd97502fa7ee01
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-02-27 21:39:19 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,29 +1,72 @@
-locals {
-  backup_policies = {
-    for vault_key, vault in azurerm_data_protection_backup_vault.backup_vault :
-    vault_key => {
-      vault_name = vault.name
-    }
-  }
+############################################################
+# Trusted Access Binding (Vault -> AKS)
+############################################################
+
+resource "azurerm_kubernetes_cluster_trusted_access_role_binding" "aks_trusted_access" {
+  for_each = local.backup_instances
+
+  name                  = "backup-${each.key}"
+  kubernetes_cluster_id = module.aks_cluster[each.key].id
+  roles                 = ["Microsoft.DataProtection/backupVaults/backup-operator"]
+  source_resource_id    = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].id
 }
 
-resource "azurerm_data_protection_backup_policy_kubernetes_cluster" "aks_policy" {
-  for_each = local.backup_policies
+############################################################
+# Vault MSI -> Reader on AKS Cluster
+############################################################
 
-  name                = "aks-4hour-policy"
-  resource_group_name = local.conf.settings.resource_group_name
-  vault_name          = each.value.vault_name
+resource "azurerm_role_assignment" "vault_reader_on_cluster" {
+  for_each = local.backup_instances
 
-  # Every 4 hours
-  backup_repeating_time_intervals = [
-    "R/2025-01-01T00:00:00+00:00/PT4H"
-  ]
-  time_zone                  = "UTC"
+  scope                = module.aks_cluster[each.key].id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].identity[0].principal_id
+}
 
-  default_retention_rule {
-    life_cycle {
-      duration        = "P30D"
-      data_store_type = "OperationalStore"
-    }
-  }
+############################################################
+# Vault MSI -> Disk Snapshot Contributor (Node RG)
+############################################################
+
+resource "azurerm_role_assignment" "vault_snapshot_contributor" {
+  for_each = local.backup_instances
+
+  scope                = azurerm_resource_group.snapshot_rg[each.key].id
+  role_definition_name = "Disk Snapshot Contributor"
+  principal_id         = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].identity[0].principal_id
+}
+
+############################################################
+# Vault MSI -> Data Operator for Managed Disks (Node RG)
+############################################################
+
+resource "azurerm_role_assignment" "vault_data_operator" {
+  for_each = local.backup_instances
+
+  scope                = azurerm_resource_group.snapshot_rg[each.key].id
+  role_definition_name = "Data Operator for Managed Disks"
+  principal_id         = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].identity[0].principal_id
+}
+
+############################################################
+# Vault MSI -> Storage Blob Data Contributor
+############################################################
+
+resource "azurerm_role_assignment" "vault_blob_data_contributor" {
+  for_each = local.backup_instances
+
+  scope                = azurerm_storage_account.storage_account[each.value.storage_account].id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].identity[0].principal_id
+}
+
+############################################################
+# AKS Backup Extension Identity -> Storage Account Contributor
+############################################################
+
+resource "azurerm_role_assignment" "extension_storage_contributor" {
+  for_each = local.backup_instances
+
+  scope                = azurerm_storage_account.storage_account[each.value.storage_account].id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_kubernetes_cluster_extension.aks_backup[each.key].aks_assigned_identity[0].principal_id
 }
