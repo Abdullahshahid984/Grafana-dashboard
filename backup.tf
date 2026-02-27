7cf44bff062c5722e32416a2d02f909c1ae02b34
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-02-27 21:37:51 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,26 +1,29 @@
-resource "azurerm_data_protection_backup_instance_kubernetes_cluster" "aks_backup_instance" {
-  for_each = local.backup_instances
-
-  name                         = "bi-${each.key}"
-  location                     = local.conf.settings.location
-  vault_id                     = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].id
-  kubernetes_cluster_id        = module.aks_cluster[each.key].id
-  snapshot_resource_group_name = azurerm_resource_group.snapshot_rg[each.key].name
-  backup_policy_id             = azurerm_data_protection_backup_policy_kubernetes_cluster.aks_policy[each.value.backup_vault].id
-
-  backup_datasource_parameters {
-    cluster_scoped_resources_enabled = true
-    volume_snapshot_enabled          = true
-
-    # FULL CLUSTER PROTECTION 
-    included_namespaces     = []
-    excluded_namespaces     = []
-    included_resource_types = []
-    excluded_resource_types = []
-    label_selectors         = []
+locals {
+  backup_policies = {
+    for vault_key, vault in azurerm_data_protection_backup_vault.backup_vault :
+    vault_key => {
+      vault_name = vault.name
+    }
   }
+}
 
-  depends_on = [
-    azurerm_kubernetes_cluster_extension.aks_backup
+resource "azurerm_data_protection_backup_policy_kubernetes_cluster" "aks_policy" {
+  for_each = local.backup_policies
+
+  name                = "aks-4hour-policy"
+  resource_group_name = local.conf.settings.resource_group_name
+  vault_name          = each.value.vault_name
+
+  # Every 4 hours
+  backup_repeating_time_intervals = [
+    "R/2025-01-01T00:00:00+00:00/PT4H"
   ]
+  time_zone                  = "UTC"
+
+  default_retention_rule {
+    life_cycle {
+      duration        = "P30D"
+      data_store_type = "OperationalStore"
+    }
+  }
 }
