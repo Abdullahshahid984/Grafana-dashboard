0cee9a10eb60bfa859c579f4b10121992e1b08d9
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-04-14 22:45:23 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,12 +1,22 @@
-# Get AKS cluster user-assigned identity
-data "azurerm_user_assigned_identity" "aks_identity" {
-  for_each = local.backup_instances
+ cluster_identities = {
+    for ci in lookup(local.conf, "cluster_identity", []) :
+    ci.name => ci.name
+  }
 
-  name                = "mi-bfhaks-ihub_cluster-eus2-poc-01"
+  aks_to_identity = {
+    for aks in lookup(local.conf, "aks_cluster", []) :
+    aks.name => aks.identity.identity_ids[0].ref
+  } 
+
+data "azurerm_user_assigned_identity" "aks_identity" {
+  for_each = local.aks_to_identity
+
+  name                = each.value
   resource_group_name = local.conf.settings.resource_group_name
 }
 
-# Give AKS identity Contributor on snapshot RG
+
+
 resource "azurerm_role_assignment" "aks_contributor_on_snapshot_rg" {
   for_each = local.backup_instances
 
