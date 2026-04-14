21a5f17b4732ad77a68fb2af1b81a0ec8f0e9332
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-04-14 22:59:57 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,1 +1,1 @@
-terraform import module.bfhaksplatform.azurerm_role_assignment.aks_contributor_on_snapshot_rg["aks-bfhaks-ihub-eus2-poc-01"] /subscriptions/<sub-id>/providers/Microsoft.Authorization/roleAssignments/fd2d7c2f-5659e0e80ccc86341a55a49
+terraform import 'module.bfhaksplatform.azurerm_role_assignment.aks_contributor_on_snapshot_rg["aks-bfhaks-ihub-eus2-poc-01"]' /subscriptions/1fd4eaa2-e0f5-4e47-a395-12bad3a0cd73/providers/Microsoft.Authorization/roleAssignments/fd2d7c2f-5659e0e80ccc86341a55a49
