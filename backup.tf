c8da95d5642ca8e0273499c006448e4bd1c6e2ae
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-04-14 22:40:09 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,3 +1,16 @@
-module.aks_cluster[each.key].kubelet_identity[0].object_id
-# or
-module.aks_cluster[each.key].identity[0].principal_id
+# Get AKS cluster user-assigned identity
+data "azurerm_user_assigned_identity" "aks_identity" {
+  for_each = local.backup_instances
+
+  name                = "mi-bfhaks-ihub_cluster-eus2-poc-01"
+  resource_group_name = local.conf.settings.resource_group_name
+}
+
+# Give AKS identity Contributor on snapshot RG
+resource "azurerm_role_assignment" "aks_contributor_on_snapshot_rg" {
+  for_each = local.backup_instances
+
+  scope                = data.azurerm_resource_group.snapshot_rg.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_user_assigned_identity.aks_identity[each.key].principal_id
+}
