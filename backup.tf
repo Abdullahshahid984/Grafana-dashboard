d2f5d615df5601a3f143234803e49ed18bbec831
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-04-14 22:06:07 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,17 +1,3 @@
-# Contributor on snapshot RG — required by AKS cluster managed identity
-resource "azurerm_role_assignment" "aks_cluster_contributor_on_snapshot_rg" {
-  for_each = local.backup_instances
-
-  scope                = data.azurerm_resource_group.snapshot_rg.id
-  role_definition_name = "Contributor"
-  principal_id         = module.aks_cluster[each.key].identity[0].principal_id
-}
-
-# Reader on snapshot RG — required by Backup Vault managed identity
-resource "azurerm_role_assignment" "vault_reader_on_snapshot_rg" {
-  for_each = local.backup_instances
-
-  scope                = data.azurerm_resource_group.snapshot_rg.id
-  role_definition_name = "Reader"
-  principal_id         = azurerm_data_protection_backup_vault.backup_vault[each.value.backup_vault].identity[0].principal_id
-}
+module.aks_cluster[each.key].kubelet_identity[0].object_id
+# or
+module.aks_cluster[each.key].identity[0].principal_id
