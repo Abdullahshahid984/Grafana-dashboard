14c5c3643b6f12fb97b495c3cb6f2b79bd71111f
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-06-08 21:10:23 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,24 +1,13 @@
-data "azurerm_user_assigned_identity" "cluster_identity" {
-  for_each = local.backup_instances
-
-  name                = [
-    for c in local.aks_clusters_conf :
-    c.identity.identity_ids[0].ref
-    if c.name == each.key
-  ][0]
-  resource_group_name = local.conf.settings.resource_group_name
-}
-
-
-
 ################################################################################
-# RBAC - AKS CLUSTER IDENTITY ROLE ASSIGNMENTS
+#                      Restore Clusters (Backup & Restore)
+#
+# Map of restore cluster name => OIDC issuer URL.
+# Passed in from phase 02 after reading restore cluster data sources.
+# Empty map {} means no restore federated credentials will be created.
 ################################################################################
 
-resource "azurerm_role_assignment" "cluster_contributor_on_snap_rg" {
-  for_each = local.backup_instances
-
-  scope                = data.azurerm_resource_group.snapshot_rg.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azurerm_user_assigned_identity.cluster_identity[each.key].principal_id
+variable "restore_cluster_oidc_issuer_urls" {
+  type        = map(string)
+  description = "Map of restore cluster name => OIDC issuer URL. Empty map if no restore clusters are needed."
+  default     = {}
 }
