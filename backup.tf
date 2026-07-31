c2f21375d441671228c407c99519de0f6448f0fe
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-07-31 20:43:52 +0500

    Remove commented import statement
    Removed commented import statement from backup.tf

--- backup.tf
+++ backup.tf
@@ -1,5 +1,3 @@
-# import.tf
-
 import {
   to = module.bfhaksplatform.azurerm_role_assignment.cluster_contributor_on_snap_rg
   id = "/subscriptions/1fd4eaa2-e0f5-4e47-a395-12bad3a0cd73/resourcegroups/rg-bfhaks-ihub-poc-eus2-dev-01/providers/Microsoft.Authorization/roleAssignments/fd2d7c2f-d569-5e0e-80cc-c86341a55a49"
