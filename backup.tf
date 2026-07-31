b7984f3e40791fc6bd928b419deb14d61cda2251
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-07-31 21:02:19 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,4 +1,4 @@
 import {
-  to = module.bfhaksplatform.azurerm_role_assignment.cluster_contributor_on_snap_rg
+  to = module.bfhaksplatform.azurerm_role_assignment.cluster_contributor_on_snap_rg["aks-bfhaks-ihub-eus2-poc-01"]
   id = "/subscriptions/1fd4eaa2-e0f5-4e47-a395-12bad3a0cd73/resourcegroups/rg-bfhaks-ihub-poc-eus2-dev-01/providers/Microsoft.Authorization/roleAssignments/fd2d7c2f-d569-5e0e-80cc-c86341a55a49"
 }
