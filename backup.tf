a4fbe2ce4db22b8008256145c7d7d547ac1953c9
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-09-01 21:53:37 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,5 +1,4 @@
-variable "excluded_cluster_names" {
-  description = "List of AKS cluster names to exclude from Prometheus alert rule group creation."
-  type        = list(string)
-  default     = []
+import {
+  to = azurerm_dashboard_grafana.grafana
+  id = "/subscriptions/1fd4eaa2-e0f5-4e47-a395-12bad3a0cd73/resourceGroups/rg-bfhaks-ihub-eus2-dev-01/providers/Microsoft.Dashboard/grafana/bfhaksihubeus2dev01"
 }
