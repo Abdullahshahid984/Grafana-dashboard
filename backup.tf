821cd75fbb303e27f00a0e68c7059deef9d87532
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-09-16 20:00:30 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,16 +1,12 @@
-FILE 4: kubernetes-resources/main.tf
-Line 40 - CHANGE:
+################################################################################
+#                    Restore/Validation Cluster Detection - NEW
+#
+# When true: Alert rule names include cluster name (prevent conflicts)
+# When false: Original alert rule names (backward compatible)
+################################################################################
 
-# FROM: for_each = local.app_team_principals_for_this_instance
-# TO:
-  for_each = local.app_team_principals_final
-Line 50 - CHANGE:
-
-# FROM: for_each = local.app_team_principal_all_namespace_for_this_instance
-# TO:
-  for_each = local.app_team_principal_all_namespace_final
-Line 60 - CHANGE:
-
-# FROM: for_each = local.devops_pipeline_credentials_for_this_instance[local.matching_platform_instance_data.devops_pipeline_credential.type]
-# TO:
-  for_each = local.devops_pipeline_credentials_final[local.matching_platform_instance_data.devops_pipeline_credential.type]
+variable "is_restore_cluster" {
+  type        = bool
+  description = "Set to true for restore/validation clusters (e.g., dev-rst). Adds cluster name to alert rule names."
+  default     = false
+}
