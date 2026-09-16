6291f536fb7608f091518c1f9b34b245dbd9fbeb
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-09-17 02:08:21 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,3 +1,11 @@
-  additional_cluster_oidc_issuer_urls = {
-    for k, v in data.azurerm_kubernetes_cluster.additional_clusters : k => v.oidc_issuer_url
+locals {
+  federated_credential_subject = { for k, v in local.computed_component_map : k =>
+    join(":", [
+      "system",
+      "serviceaccount",
+      kubernetes_namespace_v1.ns.metadata.0.name,
+      kubernetes_service_account_v1.workload_identity[k].metadata.0.name
+    ])
+    if v.workload_identity == true
   }
+}
