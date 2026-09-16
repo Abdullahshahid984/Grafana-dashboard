38fef536115e4a43e81fb6dcee590f2521b2ebb8
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2026-09-16 19:56:12 +0500

    Update backup.tf

--- backup.tf
+++ backup.tf
@@ -1,36 +1,16 @@
+FILE 4: kubernetes-resources/main.tf
+Line 40 - CHANGE:
 
-################################################################################
-#                    RBAC Keys: Conditional by Cluster Type - NEW
-#
-# IF is_restore_cluster = true:  Use cluster-specific keys (prevent conflicts)
-# IF is_restore_cluster = false: Use original keys (backward compatible)
-################################################################################
+# FROM: for_each = local.app_team_principals_for_this_instance
+# TO:
+  for_each = local.app_team_principals_final
+Line 50 - CHANGE:
 
-locals {
-  # Extract cluster name from Azure resource ID
-  # Format: /subscriptions/.../providers/Microsoft.ContainerService/managedClusters/aks-cluster-name
-  cluster_name = reverse(split("/", var.kubernetes_cluster_id))[0]
+# FROM: for_each = local.app_team_principal_all_namespace_for_this_instance
+# TO:
+  for_each = local.app_team_principal_all_namespace_final
+Line 60 - CHANGE:
 
-  # Conditional RBAC keys based on cluster type
-  app_team_principals_final = var.is_restore_cluster ? {
-    for principal_name, principal_config in local.app_team_principals_for_this_instance :
-    "${principal_name}__${local.cluster_name}" => principal_config
-  } : local.app_team_principals_for_this_instance
-
-  app_team_principal_all_namespace_final = var.is_restore_cluster ? {
-    for principal_name, principal_config in local.app_team_principal_all_namespace_for_this_instance :
-    "${principal_name}__${local.cluster_name}" => principal_config
-  } : local.app_team_principal_all_namespace_for_this_instance
-
-  devops_pipeline_credentials_final = var.is_restore_cluster ? {
-    none = {}
-    custom = {
-      for i in lookup(local.matching_platform_instance_data.devops_pipeline_credential, "custom", null) :
-      "${i.name}__${local.cluster_name}" => i
-    }
-    instance_default = {
-      for i in var.default_devops_pipeline_credential :
-      "${i.name}__${local.cluster_name}" => i
-    }
-  } : local.devops_pipeline_credentials_for_this_instance
-}
+# FROM: for_each = local.devops_pipeline_credentials_for_this_instance[local.matching_platform_instance_data.devops_pipeline_credential.type]
+# TO:
+  for_each = local.devops_pipeline_credentials_final[local.matching_platform_instance_data.devops_pipeline_credential.type]
